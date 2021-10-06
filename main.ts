f8d286208d1dd52be4e8d0959280bb782cdfca9a
482798 <90925518+482798@users.noreply.github.com>
2021-10-06 09:38:35 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,6 +4,7 @@ input.onButtonPressed(Button.A, function () {
 input.onButtonPressed(Button.AB, function () {
     if (password == input2) {
         basic.showIcon(IconNames.Yes)
+        music.playMelody("G B A G C5 B A B ", 120)
         basic.pause(500)
         input2 = ""
         basic.clearScreen()
