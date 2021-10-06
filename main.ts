1ff946e7e28292c539a1bce060d79e5b59b1ed41
482798 <90925518+482798@users.noreply.github.com>
2021-10-06 09:08:07 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,20 @@
-basic.forever(function () {
-	
+let _1 = 0
+input.onButtonPressed(Button.A, function () {
+    _1 += 1
+})
+input.onButtonPressed(Button.AB, function () {
+    if (_1 == 7) {
+        basic.showIcon(IconNames.Yes)
+        basic.pause(500)
+        _1 = 0
+        basic.clearScreen()
+    } else {
+        basic.showIcon(IconNames.No)
+        basic.pause(500)
+        _1 = 0
+        basic.clearScreen()
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    _1 += 2
 })
