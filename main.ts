b9e0cd4ff7ef69a5ef3736c0cc14ce88dea3d285
482798 <90925518+482798@users.noreply.github.com>
2021-10-13 09:39:11 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,26 +1,26 @@
 input.onButtonPressed(Button.A, function () {
-    user_input = "" + user_input + "A"
+    User_Input = "" + User_Input + "A"
 })
 input.onButtonPressed(Button.AB, function () {
-    if (password == user_input) {
+    if (Password == User_Input) {
         basic.showIcon(IconNames.Yes)
         pins.servoWritePin(AnalogPin.P0, 180)
         basic.pause(5000)
         pins.servoWritePin(AnalogPin.P0, 0)
-        user_input = ""
+        User_Input = ""
         basic.clearScreen()
     } else {
         pins.servoWritePin(AnalogPin.P0, 0)
         basic.showIcon(IconNames.No)
         basic.pause(500)
-        user_input = ""
+        User_Input = ""
         basic.clearScreen()
     }
 })
 input.onButtonPressed(Button.B, function () {
-    user_input = "" + user_input + "B"
+    User_Input = "" + User_Input + "B"
 })
-let user_input = ""
-let password = ""
-password = "ABBAB"
-user_input = ""
+let User_Input = ""
+let Password = ""
+Password = "ABBAB"
+User_Input = ""
