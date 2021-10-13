78cf4f22cbab5d22a83626942700dbb9a74f1829
482798 <90925518+482798@users.noreply.github.com>
2021-10-13 09:54:31 -0700

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -4,17 +4,25 @@ def on_button_pressed_a():
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def on_button_pressed_ab():
-    global User_Input
+    global User_Input, _1
     if Password == User_Input:
-        basic.show_icon(IconNames.YES)
-        pins.servo_write_pin(AnalogPin.P0, 180)
-        basic.pause(5000)
-        pins.servo_write_pin(AnalogPin.P0, 0)
-        User_Input = ""
-        basic.clear_screen()
+        if _1 > 4:
+            pins.servo_write_pin(AnalogPin.P0, 0)
+            basic.show_icon(IconNames.NO)
+            basic.pause(300)
+            User_Input = ""
+            basic.clear_screen()
+        else:
+            basic.show_icon(IconNames.YES)
+            pins.servo_write_pin(AnalogPin.P0, 180)
+            basic.pause(5000)
+            pins.servo_write_pin(AnalogPin.P0, 0)
+            User_Input = ""
+            basic.clear_screen()
     else:
         pins.servo_write_pin(AnalogPin.P0, 0)
         basic.show_icon(IconNames.NO)
+        _1 += 1
         basic.pause(300)
         User_Input = ""
         basic.clear_screen()
@@ -27,5 +35,16 @@ input.on_button_pressed(Button.B, on_button_pressed_b)
 
 User_Input = ""
 Password = ""
+_1 = 0
 Password = "ABBAB"
 User_Input = ""
+_1 = 0
+
+def on_forever():
+    global _1
+    _1 = 0
+    basic.pause(5000)
+    basic.pause(5000)
+    basic.pause(5000)
+    basic.pause(5000)
+basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -5,15 +5,25 @@ input.onButtonPressed(Button.A, function on_button_pressed_a() {
 input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
     
     if (Password == User_Input) {
-        basic.showIcon(IconNames.Yes)
-        pins.servoWritePin(AnalogPin.P0, 180)
-        basic.pause(5000)
-        pins.servoWritePin(AnalogPin.P0, 0)
-        User_Input = ""
-        basic.clearScreen()
+        if (_1 > 4) {
+            pins.servoWritePin(AnalogPin.P0, 0)
+            basic.showIcon(IconNames.No)
+            basic.pause(300)
+            User_Input = ""
+            basic.clearScreen()
+        } else {
+            basic.showIcon(IconNames.Yes)
+            pins.servoWritePin(AnalogPin.P0, 180)
+            basic.pause(5000)
+            pins.servoWritePin(AnalogPin.P0, 0)
+            User_Input = ""
+            basic.clearScreen()
+        }
+        
     } else {
         pins.servoWritePin(AnalogPin.P0, 0)
         basic.showIcon(IconNames.No)
+        _1 += 1
         basic.pause(300)
         User_Input = ""
         basic.clearScreen()
@@ -26,5 +36,15 @@ input.onButtonPressed(Button.B, function on_button_pressed_b() {
 })
 let User_Input = ""
 let Password = ""
+let _1 = 0
 Password = "ABBAB"
 User_Input = ""
+_1 = 0
+basic.forever(function on_forever() {
+    
+    _1 = 0
+    basic.pause(5000)
+    basic.pause(5000)
+    basic.pause(5000)
+    basic.pause(5000)
+})
